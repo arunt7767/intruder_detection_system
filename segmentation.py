785753fd4cce716e977246febb01d15ef25eaf89
Arun singh thakur <arunt7767@gmail.com>
2023-09-01 03:42:11 +0530

    Add files via upload

---
+++ segmentation.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+from ultralytics.yolo.v8.detect.predict import DetectionPredictor
+import cv2
+
+model = YOLO("yolov8n-seg.pt")
+results = model.predict(source = "0" , show = True)
+print(results)
